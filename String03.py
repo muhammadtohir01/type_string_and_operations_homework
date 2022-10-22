0f498530f1598530c241f4f887e3907f4baac47a
muhammadtohir01 <nabiyevmuhammadtohir@gmail.com>
2022-10-22 17:07:40 +0500

    3 misol

--- String03.py
+++ String03.py
@@ -1,3 +1,7 @@
+from re import A
+from traceback import print_tb
+
+
 def main():
     """
     "*" Return a string that consists of only this character and is five in length.
@@ -6,4 +10,6 @@ def main():
     Returns:
         str: return answer.
     """
-    return 
+    a='*****'
+    return a 
+print(main())
